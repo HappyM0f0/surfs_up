d34e568f82d02d6b39555f17b5d30b99591d44d6
Vynstan <HappyM0f0@icloud.com>
2021-10-11 19:17:51 -0500

    preclass updates

---
+++ app.py
@@ -0,0 +1,39 @@
+# from flask import Flask
+# app = Flask(__name__)
+# @app.route('/')
+# def hello_world():
+#     return 'Hello world'
+
+## import dependencies
+import datetime as dt
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
+# Set Up the Database
+engine = create_engine("sqlite:///hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+session = Session(engine)
+
+# setting up flash location
+app = Flask(__name__)
+
+#setting up routes
+@app.route("/")
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
+
